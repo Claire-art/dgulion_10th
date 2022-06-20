0d1b13f5b5c91302229fb9422dab6fbd1e2225a3
panghyuk <okh9712@gmail.com>
2022-06-20 18:22:41 +0900

    세션 준비

---
+++ main/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    writer=models.ForeignKey(User,on_delete=models.CASCADE)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image= models.ImageField(upload_to="blog/",blank=True,null=True)
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:20]
+    
+class Comment(models.Model):
+    content = models.TextField()
+    writer = models.ForeignKey(User, on_delete=models.CASCADE)
+    blog = models.ForeignKey( Blog ,on_delete=models.CASCADE, related_name ='comments')
+    created_at = models.DateTimeField(auto_now=True)
+    update_at = models.DateTimeField(auto_now=True)
---
+++ main/urls.py
@@ -0,0 +1,16 @@
+
+from django.urls import path
+from .views import *
+
+app_name = "main"
+urlpatterns = [
+    path('', main, name="showmain"),
+    path('<str:id>', detail, name="detail"),
+    path('new/', new, name="new"),
+    path('create/', create, name="create"),
+    path('posts/', posts, name="posts"),
+    path('edit/<str:id>',edit,name="edit"),
+    path('update/<str:id>',update,name="update"),
+    path('delete/<str:id>',delete,name="delete"),
+    path('<str:blog_id>/create_comment', create_comment, name="create_comment"),
+]
